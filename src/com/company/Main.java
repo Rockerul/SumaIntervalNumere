88bd656f478183527fb447b1dc41a6dfeb9998cb
Tester1 <pau.rockerul@gmail.com>
2022-07-12 14:38:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Suma  numerelor cumprinse intr-un interval de 2 numere
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduceti primul numar :");
+        int a = scanner.nextInt();
+        System.out.println("Introduceti al2lea numar :");
+        int b = scanner.nextInt();
+
+        if (a < b) {
+            int sum = 0;
+
+            for (int i = a; i <= b; i++) {
+                sum += i;
+
+            }
+
+            System.out.println("Suma numerelor cuprinse intre " + a + " si " + b + " este = " + sum);
+        } else {
+            System.out.println("ERROR!!!");
+        }
+
+    }
+}
+
+
